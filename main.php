b1eecda90fd3cb5c4f529e8a338854cea9861078
Nehemiah Emmanuel <111321633+GISTechHUB@users.noreply.github.com>
2022-11-16 05:03:57 +0100

    Add files via upload

---
+++ main.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System By Group 58</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+   <div class="text-center">
+      <h3> <a class="small" href="../index.html" style="font-weight:bold;"><i class="fa fa-home" aria-hidden="true"></i> MAIN MENU</a></h3>
+    </div>
+    <div class="container">
+
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid19-TMS ~ By Group 58 IMAP</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Covid-19 Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>COVID19-TMS </h1>
+      <p class="lead">COVID19 - Testing Management System  ||  By Group 58 IMAP -~ COMP SC.</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+         
+         <h2><b><strong>About this page</strong></b> </h2>
+          <hr>
+          <p class="lead">Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19, virus will experience mild to moderate, respiratory illness & recover without requiring special treatment. Older people and those with underlying medical problem like cardiovascular disease.</p>
+          <p class="lead">The COVID-19 virus spread primarily through droplet of saliva or discharge from the nose when an infected person coughs or sneezes so it’s important that you also practice respiratory etiquette.</p><p class="lead">
+         
+         <b><strong>Types of COVID-19 Tests</strong></b> 
+         </p><p class="lead"><hr>
+         <b><strong><i>On this page:</i></strong></b> 
+
+</p><p class="lead">
+Testing for current infection<br>
+Testing for past infection
+</p><p class="lead">
+
+<i> Testing for current infection</i>
+Viral tests, sometimes called diagnostic tests, can detect if you have SARS-CoV-2, the virus that causes COVID-19. There are two types of diagnostic tests: molecular and antigen.
+</p><p class="lead">
+
+If you have questions about which test is right for you, talk to a health care provider.
+</p><p class="lead">
+
+A positive viral COVID-19 test (molecular or antigen test) means the person who took the test has COVID-19 and can spread it to others. If you get a positive test result, you should stay home and away from others. This advice does not change if you get a second test that is negative. Refer to If You Are Sick or Test Positive: COVID-19.
+</p><p class="lead">
+
+It generally is not recommended that people get tested again after getting a positive result. However, those who work in health care and long-term care should follow testing recommendations specific to those settings. Refer to Health Advisory: Antigen-based Tests for Detection of SARS-CoV-2 (PDF).
+</p><p class="lead">
+
+
+<b><strong>Molecular tests<i>(aka RNA or PCR test)</i> </strong></b> 
+<hr>
+PCR, nucleic acid amplification tests (NAATs), and other molecular amplification tests detect the virus's genetic material.
+</p><p class="lead">
+These diagnostic tests are considered the most sensitive for detecting an active infection, and the results are highly accurate. You might take one if you or your doctor think you have COVID. You might also be asked to take this type of test if you need to prove to your employer or your college that you are not currently infected prior to returning to work or campus
+</p><p class="lead">
+
+Molecular tests are the most accurate tests for detecting the virus that causes COVID-19.
+They can be used whether or not you have symptoms.
+They are given with a nasal swab, oral (throat) swab, or by taking a saliva sample.
+</p><p class="lead">
+
+<b>Nasal swab:</b>  A nasal swabs looks like a long Q-tip. It is inserted about two inches into your nose and swirled around for a few seconds. The swab is then removed and sent to a lab for testing. Nasal swabs are fast and accurate, and they’re a good option for most people. You may experience a tickling sensation while the swab is in your nose, and after it is removed, you might sneeze or have runny eyes for a moment or two. Health care providers are more likely to use nasal swabs that go farther into your nasal cavity, and you may feel more discomfort than when doing an at-home nasal swab.<br>
+</p><p class="lead">
+
+<b> Saliva test:</b> Saliva tests are self-administered; this means that after you are shown how to perform the test, you’ll do it by yourself. You will spit several times into a funnel attached to a tube, and then screw on a cap to complete the test. If you are at a community testing site, you will then hand your sample to a supervisor; if you are performing the test at home, you will put the sample into a prepaid UPS envelope and send it out. 
+</p><p class="lead">
+Most people need 10-12 minutes to make enough spit to fill the tube. Saliva tests are more comfortable than nasal swabs and just as accurate, but they may not be a good option for those with low saliva production, such as very young children or those who have suffered a stroke.<br>
+A positive PCR, NAAT, or other molecular amplification test result is considered a confirmed case of COVID-19.
+</p><p class="lead">
+
+<b><strong>Antigen tests<i>(aka rapid test)</i> </strong></b> 
+<hr/>
+
+This type of diagnostic test is often called a "rapid test" because the turnaround time is much quicker than an RNA test. It's also cheaper to produce. As a result, antigen tests are being used to screen large numbers of people, like at airports, a recent article in the journal Nature points out.
+Antigen tests, sometimes called rapid tests, look for specific proteins on the surface of the virus.
+</p><p class="lead">
+
+Antigen tests produce results more quickly than other tests.
+They are given with a nasal swab.<br>
+<b><i>A positive antigen test result means a person has COVID-19.</i></b> 
+For more information on rapid tests that can be done at home, visit COVID-19 Self-Testing.
+</p><p class="lead">
+
+<b><strong><i>Testing for past infection</i></strong></b> 
+
+<br>
+<b><strong>Antibody test<i>(aka serology test or blood test)</i> </strong></b> 
+<hr>
+
+This test looks for antibodies to the coronavirus. Antibodies are proteins your immune system produces to fight off a foreign invader, such as a virus. A COVID-19 antibody test cannot diagnose active coronavirus infection. All it tells you is whether you've been infected at some point in the past, even if that occurred months ago. Antibodies do not become detectable until at least several days after an infection has started.
+
+</p><p class="lead">
+
+Test blood collected by a finger stick or blood draw.
+A positive antibody test means a person may have antibodies from getting COVID-19 in the past or from COVID-19 vaccine.
+What antibody tests cannot tell us
+</p><p class="lead">
+
+An antibody test cannot tell you if you currently have COVID-19. If you receive a negative antibody test, it does not mean you do not currently have COVID-19. A molecular or antigen test must be used to determine if you have COVID-19 right now.<br>
+</p><p class="lead">
+
+Antibody tests cannot tell if someone can get COVID-19 again. We do not know yet how long antibodies for the virus that causes COVID-19 last or if they can keep people from getting it again.
+Most antibody tests cannot distinguish between antibodies acquired through infection versus vaccination.</p>
+       
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>Hight Fever 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+             <hr />
+<p><strong>Dry Cough 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+          <hr />
+<p><strong>Shortness of breath!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Wash your Hands often</li>
+            <li>Wear A Face mask</li>
+            <li>Avoid contact with sick people</li>
+            <li>Always cover your cough or sneeze</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; GISTech 2022 ~ By Group 58 IMAP</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
